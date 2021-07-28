d73fa9e7ef22bc5d62dcccb18212c6672a7c9834
Yohan LEE <alien2327@gmail.com>
2021-07-28 14:22:19 +0900

    Add files via upload

---
+++ excute_sim.py
@@ -0,0 +1,4 @@
+import subprocess as sub
+
+sub.call("./16PU_Fixed.exe moment_2019_12_04_16_54_25_address13.dat moment_2019_12_04_16_54_25_address13.dat 190 251")
+sub.call("./16PU_Fixed.exe moment_2019_12_04_16_54_25_address15.dat moment_2019_12_04_16_54_25_address15.dat 150 251")
